8f9d4a7efbac128cd2c489777dd5a66d8248336a
Fadi Aboona <fadi.aboona23@gmail.com>
2023-04-07 21:52:08 -0400

    incomplete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const findMatching = (names, string) => {
+	return names.filter(names => names.toUpperCase() === string.toUpperCase());
+}
+
+
+
+const fuzzyMatch = (names, string) => {
+	return names.filter(names => names.slice() === string.slice())
+}
+
+
+const matchName = (driver, sting) => {
+	return driver.filter(driver => driver.slice === string.slice())
+}
